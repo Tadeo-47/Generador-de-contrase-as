4ce7628c752ee9e5c81fb6ba7e6bf417b7211bde
Tadeo-47 <taparedesac@uide.edu.ec>
2026-06-21 19:01:11 -0500

    Implementar generación básica de contraseñas

--- src/generador.py
+++ src/generador.py
@@ -1,1 +1,16 @@
-print("Generador Seguro de Contraseñas")
+import random
+import string
+
+print("=== GENERADOR DE CONTRASEÑAS ===")
+
+longitud = int(input("Ingrese la longitud de la contraseña: "))
+
+caracteres = string.ascii_letters + string.digits + string.punctuation
+
+contrasena = ""
+
+for i in range(longitud):
+    contrasena += random.choice(caracteres)
+
+print("\nContraseña generada:")
+print(contrasena)
